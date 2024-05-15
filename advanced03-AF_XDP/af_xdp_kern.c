9104a85edbfd46da9f16cd263a3731f73526bc22
sawhiqbr <kabar.safa@gmail.com>
2024-05-15 15:44:28 +0300

    working version of only udp bypassing

--- advanced03-AF_XDP/af_xdp_kern.c
+++ advanced03-AF_XDP/af_xdp_kern.c
@@ -8,9 +8,8 @@
 #include <linux/in.h>
 #include <bpf/bpf_endian.h>
 
-// #include <linux/types.h>
-
-#define TARGET_IP 0x907A894F // Replace with target IP in hex eg. 144.122.137.79
+// Use the bridge IP since NAT is altering the source IP
+#define SOURCE_IP 0xC0A87A01 // Replace with target IP in hex eg. 192.168.122.1
 #define TARGET_PORT 12345    // Replace with target port
 
 struct
@@ -31,53 +30,52 @@ int xdp_sock_prog(struct xdp_md *ctx)
     struct iphdr *iph;
     struct udphdr *udph;
 
-    // Debug: Packet received
     bpf_trace_printk("Packet received on queue %d\n", sizeof("Packet received on queue %d\n"), index);
 
-    // Check if packet data is at least the size of an Ethernet header
+    // packet data is at least the size of an Ethernet header
     if (data + sizeof(struct ethhdr) > data_end) {
         bpf_trace_printk("Packet size is smaller than Ethernet header\n", sizeof("Packet size is smaller than Ethernet header\n"));
         return XDP_PASS;
     }
 
-    // Check if the Ethernet frame is an IP packet
+    // Ethernet frame is an IP packet
     if (eth->h_proto != bpf_htons(ETH_P_IP)) {
         bpf_trace_printk("Ethernet frame is not IP, protocol: %x\n", sizeof("Ethernet frame is not IP, protocol: %x\n"), bpf_ntohs(eth->h_proto));
         return XDP_PASS;
     }
 
-    // Check if packet data is at least the size of an IP header
+    // packet data is at least the size of an IP header
     iph = data + sizeof(struct ethhdr);
     if ((void *)iph + sizeof(struct iphdr) > data_end) {
         bpf_trace_printk("Packet size is smaller than IP header\n", sizeof("Packet size is smaller than IP header\n"));
         return XDP_PASS;
     }
 
-    // Check if the IP packet is a UDP packet
+    // IP packet is a UDP packet
     if (iph->protocol != IPPROTO_UDP) {
         bpf_trace_printk("IP packet is not UDP, protocol: %x\n", sizeof("IP packet is not UDP, protocol: %x\n"), iph->protocol);
         return XDP_PASS;
     }
 
-    // Check if the source IP address matches the target IP address
-    __u32 src_ip = bpf_ntohl(iph->saddr); // Convert IP address to host byte order
-    bpf_trace_printk("IP src: %x, expected: %x\n", sizeof("IP src: %x, expected: %x\n"), src_ip, bpf_ntohl(TARGET_IP));
-    if (src_ip != bpf_ntohl(TARGET_IP))
+    // source IP address matches the target IP address
+    // notice due to address translation IP address is given as 192.168.122.1
+    bpf_trace_printk("IP src: %x, expected: %x\n", sizeof("IP src: %x, expected: %x\n"), src_ip, bpf_ntohl(SOURCE_IP));
+    if (iph->saddr != bpf_ntohl(SOURCE_IP))
         return XDP_PASS;
 
-    // Check if packet data is at least the size of a UDP header
+    // packet data is at least the size of a UDP header
     udph = (void *)iph + sizeof(struct iphdr);
     if ((void *)udph + sizeof(struct udphdr) > data_end) {
         bpf_trace_printk("Packet size is smaller than UDP header\n", sizeof("Packet size is smaller than UDP header\n"));
         return XDP_PASS;
     }
 
-    // Check if the destination port matches the target port
+    // destination port matches the target port
     bpf_trace_printk("UDP dest port: %d, expected: %d\n", sizeof("UDP dest port: %d, expected: %d\n"), bpf_ntohs(udph->dest), TARGET_PORT);
     if (udph->dest != bpf_htons(TARGET_PORT))
         return XDP_PASS;
 
-    // If the packet is UDP, from the target IP, and to the target port, redirect it
+    // packet is UDP, from the target IP, and to the target port, redirect it
     bpf_trace_printk("Packet matches criteria, redirecting\n", sizeof("Packet matches criteria, redirecting\n"));
     if (bpf_map_lookup_elem(&xsks_map, &index))
         return bpf_redirect_map(&xsks_map, index, 0);
