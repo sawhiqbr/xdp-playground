85dc4e0915f998b72363a4a974fc95f026d6c1eb
sawhiqbr <kabar.safa@gmail.com>
2024-05-15 14:02:08 +0300

    try to only redirect udp

--- advanced03-AF_XDP/af_xdp_kern.c
+++ advanced03-AF_XDP/af_xdp_kern.c
@@ -1,8 +1,17 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
 #include <linux/bpf.h>
-
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
 #include <bpf/bpf_helpers.h>
+#include <linux/in.h>
+#include <bpf/bpf_endian.h>
+
+// #include <linux/types.h>
+
+#define TARGET_IP 0x904A894F // Replace with target IP in hex eg. 144.122.137.79
+#define TARGET_PORT 12345    // Replace with target port
 
 struct
 {
@@ -12,30 +21,40 @@ struct
     __uint(max_entries, 64);
 } xsks_map SEC(".maps");
 
-struct
-{
-    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-    __type(key, __u32);
-    __type(value, __u32);
-    __uint(max_entries, 64);
-} xdp_stats_map SEC(".maps");
-
 SEC("xdp")
 int xdp_sock_prog(struct xdp_md *ctx)
 {
     int index = ctx->rx_queue_index;
-    __u32 *pkt_count;
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    struct ethhdr *eth = data;
+    struct iphdr *iph;
+    struct udphdr *udph;
 
-    pkt_count = bpf_map_lookup_elem(&xdp_stats_map, &index);
-    if (pkt_count)
-    {
-        /* We pass every other packet */
-        if ((*pkt_count)++ & 1)
-            return XDP_PASS;
-    }
+    if (data + sizeof(struct ethhdr) > data_end)
+        return XDP_PASS;
 
-    /* A set entry here means that the correspnding queue_id
-     * has an active AF_XDP socket bound to it. */
+    if (eth->h_proto != bpf_htons(ETH_P_IP))
+        return XDP_PASS;
+
+    iph = data + sizeof(struct ethhdr);
+    if ((void *)iph + sizeof(struct iphdr) > data_end)
+        return XDP_PASS;
+
+    if (iph->protocol != IPPROTO_UDP)
+        return XDP_PASS;
+
+    if (iph->saddr != bpf_htonl(TARGET_IP))
+        return XDP_PASS;
+
+    udph = (void *)iph + sizeof(struct iphdr);
+    if ((void *)udph + sizeof(struct udphdr) > data_end)
+        return XDP_PASS;
+
+    if (udph->dest != bpf_htons(TARGET_PORT))
+        return XDP_PASS;
+
+    // If the packet is UDP, from the target IP, and to the target port, redirect it
     if (bpf_map_lookup_elem(&xsks_map, &index))
         return bpf_redirect_map(&xsks_map, index, 0);
 
