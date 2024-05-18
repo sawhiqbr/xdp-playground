368c1079c5677884f144bd42a07933fae2388817
sawhiqbr <kabar.safa@gmail.com>
2024-05-18 15:04:01 +0300

    initial step1 sender

--- sender.py
+++ sender.py
@@ -1,34 +1,35 @@
 import socket
+import os
 
+BUFFER_SIZE = 1024
 
-def send_udp_packet(dest_ip, port, message):
+packets = {}
+packets_acked = {}
+
+def send_udp_packet(dest_ip, port):
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sequence_number = 0
+  with open(os.path.join(os.getcwd(), "objects", "small-1.obj"), 'rb') as file:
+    file_data = file.read()
+  
+  chunk_size = BUFFER_SIZE - 15
+  total_chunks = len(file_data) // chunk_size + 1
+  
+  for i in range(0, len(file_data), chunk_size):
+    sequence_number += 1
+    message = file_data[i:i+chunk_size]
+    packet = "small-1".encode() + sequence_number.to_bytes(4, "big") + total_chunks.to_bytes(4, "big") + message
+
+    packets[sequence_number] = packet
+    packets_acked[sequence_number] = False
+  
   try:
-    sock.sendto(message.encode(), (dest_ip, port))
-    print(f"Sent message: {message}")
+    for packet in packets.items():
+      sock.sendto(packet, (dest_ip, port))
   finally:
     sock.close()
-
-
-def wait_for_ack(port):
-  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  sock.bind(('0.0.0.0', port))
-  print("Waiting for ack")
-
-  try:
-    data, addr = sock.recvfrom(1024)
-    print(f"Received: {data.decode()} from {addr}")
-  except Exception as e:
-    print(e)
-    return False
-  return True
-
 
 port = 12345
 dest_ip = '192.168.122.192'  # Guest VM IP
 
-message = 'WOAH Hello from HOST MACHINE!'
-send_udp_packet(dest_ip, port, message)
-
-if (wait_for_ack(port)):
-  print("ACK received")
+send_udp_packet(dest_ip, port)
