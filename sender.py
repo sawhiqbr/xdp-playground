cac79f52d4ee6f75012e3de0a4ae9cbd807e6326
sawhiqbr <kabar.safa@gmail.com>
2024-05-18 17:45:54 +0300

    simple sender extended

--- sender.py
+++ sender.py
@@ -2,6 +2,7 @@ import socket
 import os
 
 BUFFER_SIZE = 1024
+ACK_BUFFER_SIZE = 4
 
 packets = {}
 packets_acked = {}
@@ -9,27 +10,39 @@ packets_acked = {}
 
 def send_udp_packet(dest_ip, port):
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  ack_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  ack_sock.bind(('0.0.0.0', 12345))  # Bind to the port to listen for ACKs
+
   sequence_number = 0
   with open(os.path.join(os.getcwd(), "objects", "small-1.obj"), 'rb') as file:
     file_data = file.read()
 
-  chunk_size = BUFFER_SIZE - 15
+  chunk_size = BUFFER_SIZE - 15  # this is where the -16 problem stems from
   total_chunks = len(file_data) // chunk_size + 1
 
   for i in range(0, len(file_data), chunk_size):
     sequence_number += 1
-    message = file_data[i:i+chunk_size]
+    message = file_data[i:i + chunk_size]
     packet = "small-1".encode() + b'\x00' + sequence_number.to_bytes(4, "big") + \
-        total_chunks.to_bytes(4, "big") + message
+             total_chunks.to_bytes(4, "big") + message
 
     packets[sequence_number] = packet
     packets_acked[sequence_number] = False
 
   try:
-    for packet in packets.values():
+    for seq_num, packet in packets.items():
       sock.sendto(packet, (dest_ip, port))
+      print(f"Sent packet {seq_num}")
+
+      ack, _ = ack_sock.recvfrom(ACK_BUFFER_SIZE)
+      ack_sequence_number = int.from_bytes(ack, 'big')
+      if ack_sequence_number in packets_acked:
+        packets_acked[ack_sequence_number] = True
+        print(f"Received ACK for packet {ack_sequence_number}")
+
   finally:
     sock.close()
+    ack_sock.close()
 
 
 port = 12345
